b9472b2fb85e379a1111c94cc97b02f21148d524
Marc Chester Masakayan <141996880+mcmfire@users.noreply.github.com>
2024-08-29 13:59:37 +0800

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sky and Princess",
+  description: "The official RSVP site for Sky & Princess' wedding.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
